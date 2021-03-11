bc24260b3fd92123ecae82e9b5c6a594649c02fa
nat6 <nat6dv@gmail.com>
2021-03-11 11:51:26 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0;
+    return (array === undefined || array.length === 0) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array === undefined || array.length === 0) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (array === undefined || array.length === 0) ? 0 : (array.reduce((sum, current) => sum + current, 0) / array.length);
 }
